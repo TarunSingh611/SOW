3a783cf2feab4d5d91db8c4324aa17a85e967982
Tarun Singh <TarunSingh611@protonmail.com>
2025-08-29 12:28:54 +0530

    background image issue fixed

--- client/src/pages/TermsPage.jsx
+++ client/src/pages/TermsPage.jsx
@@ -14,23 +14,26 @@ const TermsPage = () => {
   return (
     <div className="terms-page">
       <div className="terms-background">
-      <div className="terms-container">
+          <div className='background-image-container'>
+            <img src="https://storage.123fakturera.se/public/wallpapers/sverige43.jpg" alt="" id="background-image" />
+          </div>
+        <div className="terms-container">
           <h1 className="terms-page-title">{t('terms.title')}</h1>
           <button className="terms-close-button" onClick={handleClose}>
             {t('terms.closeButton')}
           </button>
-        <div className="terms-content">
-            <div 
+          <div className="terms-content">
+            <div
               className="terms-body"
               dangerouslySetInnerHTML={{ __html: t('terms.content') }}
             />
-              </div>
+          </div>
           <button className="terms-close-button terms-bottom-button" onClick={handleClose}>
             {t('terms.closeButton')}
           </button>
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 };
 
